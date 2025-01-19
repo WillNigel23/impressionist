4b0d38f482f91a1f9018fe50c6dbe1dad916edc6
WillNigel23 <willnigelcdejesus@gmail.com>
2025-01-19 22:05:17 +0800

    Fix rails 7 compatibility

--- lib/generators/active_record/impressionist_generator.rb
+++ lib/generators/active_record/impressionist_generator.rb
@@ -4,10 +4,9 @@ module ActiveRecord
       include Rails::Generators::Migration
       source_root File.join(File.dirname(__FILE__), 'templates')
 
-      # FIX, why is this implementing rails behaviour?
       def self.next_migration_number(dirname)
         sleep 1
-        if ActiveRecord::Base.timestamped_migrations
+        if defined?(ActiveRecord.timestamped_migrations) ? ActiveRecord.timestamped_migrations : ActiveRecord::Base.timestamped_migrations
           Time.now.utc.strftime("%Y%m%d%H%M%S")
         else
           "%.3d" % (current_migration_number(dirname) + 1)
